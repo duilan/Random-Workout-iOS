092ec01dee61820efbb0242327b20d0cd752a6b7
Duilan <soyduilan@gmail.com>
2021-01-19 03:27:27 -0600

    add custom title and implement it

---
+++ RandomWorkout-iOS/Custom Views/RWTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  RWTitleLabel.swift
+//  RandomWorkout-iOS
+//
+//  Created by Adrian Cortes on 18/01/21.
+//
+
+import UIKit
+
+class RWTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

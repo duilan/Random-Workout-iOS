70d763e5e97a9d38d22be7a7b182491c1f55dc3b
duilan <soyduilan@gmail.com>
2021-05-26 00:29:32 -0500

    remove selection style in history cell

--- RandomWorkout-iOS/Custom Views/Cells/HistoryCell.swift
+++ RandomWorkout-iOS/Custom Views/Cells/HistoryCell.swift
@@ -27,6 +27,7 @@ class HistoryCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
         configure()
     }
     
